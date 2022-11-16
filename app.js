a6a4fa9cf3d88cd086fafb5e84b3e6d2be7744c4
unknown <agusstincabrera214@gmail.com>
2022-11-16 11:19:00 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(document)
+document.querySelector("h1").textContent = 'Texto desde JS'
+console.log(document.querySelector("h1"))
+
+document.querySelector('p')
+console.log(document.querySelector('h3'))
+document.querySelector('h3-dos')
+console.log(document.querySelector('h3-dos'))
+console.log(document.querySelectorId('#p-dos'))
+console.log(document.querySelectorAll('p-uno'))
